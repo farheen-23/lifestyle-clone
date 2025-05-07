f819aa653ff58c37ddf7fd1d6e2862ca4f6a893a
farheen-23 <farheenmohammad190@gmail.com>
2025-05-07 14:29:17 +0530

    add JSX for website

---
+++ App.jsx
@@ -0,0 +1,18 @@
+
+
+
+import React from 'react'
+
+import './App.css';
+
+import Mainpage from './clothstylefolder/pages/Mainpage'
+
+const App = () => {
+  return (
+    <div>
+     <Mainpage/>
+    </div>
+  )
+}
+
+export default App
